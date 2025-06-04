0f92006ec6f29ef1cb939934aca49aadedb52dd8
Tradeprofx <tradeprofx9@gmail.com>
2025-06-04 16:05:14 +0300

    Update appIdResolver.js

--- packages/bot-skeleton/src/services/api/appIdResolver.js
+++ packages/bot-skeleton/src/services/api/appIdResolver.js
@@ -1,8 +1,9 @@
 const AppIds = Object.freeze({
-    'deriv.app': '19111', // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
-    'staging.deriv.app': '19112',
-    'app.deriv.com': '19111',
-    'staging-app.deriv.com': '19112',
-});
+  "deriv.app": "19111", // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
+  "staging.deriv.app": "19112",
+  "app.deriv.com": "19111",
+  "staging-app.deriv.com": "19112",
+  "tradeprofxapp.pages.dev": "80074", // Added TradeProfx domain
+})
 
-export default AppIds;
+export default AppIds
